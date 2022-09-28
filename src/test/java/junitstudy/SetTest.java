5b87b1ea6e73d5d3659274a8cdbc0c416fbe8e45
dominiqn <dmut7691@gmail.com>
2022-09-29 01:28:54 +0900

    test(set): Set의 크기 확인 테스트 코드 추가

--- src/test/java/junitstudy/SetTest.java
+++ src/test/java/junitstudy/SetTest.java
@@ -3,6 +3,10 @@ package junitstudy;
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -17,4 +21,12 @@ public class SetTest {
     }
 
     // Test Case
+
+    @DisplayName("Set의 크기는 추가한 요소에서 중복을 제거한 개수를 반환한다.")
+    @Test
+    void size() {
+        final int actual = numbers.size();
+
+        assertThat(actual).isEqualTo(3);
+    }
 }
