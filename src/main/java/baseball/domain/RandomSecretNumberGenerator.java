9e90ddb15f3e22593355adbddbfc5ae7d7ef8baf
dominiqn <dmut7691@gmail.com>
2022-10-03 20:23:34 +0900

    fix(baseball): 요구사항대로 라이브러리를 사용하여 비밀번호 생성기 수정

--- src/main/java/baseball/domain/RandomSecretNumberGenerator.java
+++ src/main/java/baseball/domain/RandomSecretNumberGenerator.java
@@ -1,33 +1,27 @@
 package baseball.domain;
 
-import static camp.nextstep.edu.missionutils.Randoms.shuffle;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 import baseball.domain.digits.Digit;
 import baseball.domain.digits.Digits;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class RandomSecretNumberGenerator implements SecretNumberGenerator {
-    public static final List<Digit> ALL_DIGITS = Arrays.asList(
-            new Digit(1),
-            new Digit(2),
-            new Digit(3),
-            new Digit(4),
-            new Digit(5),
-            new Digit(6),
-            new Digit(7),
-            new Digit(8),
-            new Digit(9)
-    );
-
     @Override
     public Digits generate(int secretNumberSize) {
-        final List<Digit> shuffledDigits = shuffle(ALL_DIGITS);
-        final List<Digit> secretDigits = take(shuffledDigits, secretNumberSize);
+        final List<Digit> secretDigits = take(secretNumberSize);
         return new Digits(secretDigits);
     }
 
-    private List<Digit> take(List<Digit> list, int size) {
-        return list.subList(0, size);
+    private List<Digit> take(int size) {
+        final LinkedHashSet<Digit> set = new LinkedHashSet<>();
+        while (set.size() < size) {
+            final int pickedNumber = pickNumberInRange(1, 9);
+            final Digit pickedDigit = new Digit(pickedNumber);
+            set.add(pickedDigit);
+        }
+        return new ArrayList<>(set);
     }
 }
